9a0b847b09158019cf616ecc4e61372253b4ffa0
oscarwiberg <oscar.wiberg@live.se>
2020-05-10 19:57:21 +0200

    simple header

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 
 import TableWrapper from './components/TableWrapper';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header />
       <Container maxWidth="lg">
         <TableWrapper />
       </Container>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    position: 'static',
+    marginBottom: '2%',
+  },
+  heading: {
+    margin: '0 auto',
+    fontFamily: 'roboto',
+    fontSize: '1.8rem',
+    fontVariant: 'small-caps',
+  },
+});
 
 const Header = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <Box>
+      <AppBar className={classes.root}>
+        <Toolbar className={classes.heading}>
+          Covid-19 visualization - Oscar Wiberg 2020
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Header;
